175792528f6cf1d50a0d782c162a9bcbef826c9c
Elliot <hughese4@wwu.edu>
2022-05-24 22:53:17 -0700

    I dunno why there is an error, laptop version with same exact syntax doesn't have one

--- selectHand.py
+++ selectHand.py
@@ -12,13 +12,12 @@ class selectHand():
         for i in handDict.keys():
             num += 1
             card = rankSuit.randCard()
-            handDict[i] = (card[0] + " of " + card[1])
-            
+            handDict[i] = (card[0] + " of " card[1])
+
         print(handDict)
         print()
 
         cribCard1 = input("Select the first card to put into the crib (by typing the number in front it).\n> ")
         cribCard2 = input("Select the second card to put into the crib.\n> ")
-
-        #needs to call a method in bothand to select crib cards and send them to crib class
         
+        #needs to call a method in botHand to select crib cards and send them to crib class
